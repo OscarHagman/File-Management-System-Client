d80263ca85532af44496ca7c00af2a064ab70163
OscarHagman <oscar.leslie.hagman@gmail.com>
2021-10-15 15:57:38 +0200

    resetting project setup

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { Routing } from './route/Routing'
-test('inputfield', () => {
-  render(<App />);
-
-});
