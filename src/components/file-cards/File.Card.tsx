8514b51f83be8c5b68185cef68f2812b1d6071c4
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-11-16 14:02:00 +0100

    removed comment

--- src/components/file-cards/File.Card.tsx
+++ src/components/file-cards/File.Card.tsx
@@ -31,31 +31,19 @@ const FileCard: React.FC<Props> = ({ title, author, category, fileId }) => {
 			.catch((error) => console.log('ERROR:', error))
 	}
 
-	/* 	const deleteFile = () => {
-			BackendAPIService.deleteFile(fileId)
-				.then((response: any) => {
-					console.log(response)
-					onDelete(fileId)
-				})
-				.catch((error: any) => { console.log('ERROR:', error) })
-		} */
-
 	return (
 		<div>
 			<h1 className="cardTop">{title}</h1>
-
 			<div className="cardBottom">
 				<p className="textKey">Author:</p> <p className="textValue">{author}</p>
 				<p className="textKey">Category:</p> <p className="textValue">{category}</p>
 				<p className="textKey">Created At:</p> <p className="textValue">2021/10/27</p>
 				<p className="textKey">Updated At:</p> <p className="textValue">2021/10/27</p>
 			</div>
-
 			<div className="cardRight">
 				<Button text="download" action={downloadFile} />
 				<button onClick={() => deleteFile(fileId)}>delete</button>
 			</div>
-
 			<div className="cardLeft">
 				<img src="../../../public/favicon.ico" alt="file-type-icon" />
 				<Button text="Edit" action={editFile} />
