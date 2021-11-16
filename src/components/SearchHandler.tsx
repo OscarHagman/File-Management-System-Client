1ce47a470be04418a7d0410878af2d2b38028ccb
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-11-16 15:07:45 +0100

    added debounce

--- src/components/SearchHandler.tsx
+++ src/components/SearchHandler.tsx
@@ -1,14 +1,38 @@
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useFileManagement } from '../hooks/useFileManagement'
 import { SearchContext } from '../shared/provider/SearchFileProvider'
 import Button from './buttons/Button'
 import FieldInput from './inputs/Field.Input'
 import FileCardList from './file-cards/File.Card.List'
+import { useDebounce } from '../hooks/useDebounce'
 
 export const SearchHandler = () => {
 	const [searchResults] = useContext(SearchContext)
 	const [searchField, setSearchField] = useState<string>('')
+	const [isSearching, setIsSearching] = useState(false)
 	const { searchFiles } = useFileManagement()
+
+	const debouncedSearchTerm = useDebounce(searchField, 500)
+
+	const startSearch = async () => {
+		setIsSearching(true)
+		if (debouncedSearchTerm) {
+			try {
+				searchFiles(searchField)
+				setIsSearching(false)
+			} catch (error) {
+				setIsSearching(false)
+				/* 	setResults([]) */
+			}
+		} else {
+			/* setResults([]) */
+			setIsSearching(false)
+		}
+	}
+
+	useEffect(() => {
+		startSearch()
+	}, [debouncedSearchTerm])
 
 	return (
 		<>
@@ -17,10 +41,8 @@ export const SearchHandler = () => {
 					setSearchField(e.currentTarget.value.toLocaleLowerCase())
 				}
 			} />
-			<Button text="Search" action={() => searchFiles(searchField)} />
-			{searchResults && <FileCardList
-				files={searchResults}
-			/>}
+			{isSearching ? <h1>Searching..</h1> : <FileCardList files={searchResults} />}
+
 		</>
 	)
 }
