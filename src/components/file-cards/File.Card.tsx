8c02874a442b31aedc525917d68277177d2aee9e
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-11-17 08:41:44 +0100

    design update

--- src/components/file-cards/File.Card.tsx
+++ src/components/file-cards/File.Card.tsx
@@ -2,6 +2,7 @@ import Button from '../buttons/Button'
 import React from 'react'
 import BackendAPIService from '../../shared/api/service/BackendAPIService'
 import { useFileManagement } from '../../hooks/useFileManagement'
+import styled from 'styled-components'
 
 interface Props {
 	title: string,
@@ -29,21 +30,24 @@ export const FileCard: React.FC<Props> = ({ title, author, category, fileId }) =
 	}
 
 	return (
-		<div>
+		<Wrapper>
 			<h1>{title}</h1>
+			<img src="../../../public/favicon.ico" alt="file-type-icon" />
 			<div>
-				<p>Author:</p> <p>{author}</p>
-				<p>Category:</p> <p>{category}</p>
-				<p>Created At:</p> <p>2021/10/27</p>
-				<p>Updated At:</p> <p>2021/10/27</p>
+				<span>Author: {author}</span> <br />
+				<span>Category: {category}</span> <br />
+				<span>Created At: 2021/10/27</span> <br />
+				<span>Updated At: 2021/10/27</span> <br />
 			</div>
 			<div>
 				<Button text="download" action={downloadFile} />
 				<button onClick={() => deleteFile(fileId)}>delete</button>
 			</div>
-			<div>
-				<img src="../../../public/favicon.ico" alt="file-type-icon" />
-			</div>
-		</div>
+		</Wrapper>
 	)
 }
+
+const Wrapper = styled.div`
+	margin: 1%;
+	background-color: white;
+`
