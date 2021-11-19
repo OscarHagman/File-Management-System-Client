d3051b2eb022bcf4b2b5e19b269ad620c71d135b
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-11-19 11:19:10 +0100

    updated

--- src/components/file-cards/NoFileFound.tsx
+++ src/components/file-cards/NoFileFound.tsx
@@ -1,7 +1,13 @@
+import styled from "styled-components"
+
 export const NoFileFound = () => {
    return (
-      <div>
-         <img src={'https://cdn.dribbble.com/users/355650/screenshots/1487084/040913_sad-cloud_dribbble.gif'} alt={'error'} />
-      </div>
+      <Wrapper>
+         <img src={'https://images.squarespace-cdn.com/content/v1/5e3cad759f9e4472a97f32e9/1585546020818-1WV2DEPDRJ7EC83MDJ0X/Error-404.gif?format=1000w'} />
+      </Wrapper>
    )
 }
+
+const Wrapper = styled.div`
+   margin-top: 5%;
+`
