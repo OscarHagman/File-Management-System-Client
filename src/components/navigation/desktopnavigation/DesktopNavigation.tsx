7214474ece9bee8c55f52c323cf97377ba7065bd
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-11-30 10:28:53 +0100

    updated

--- src/components/navigation/desktopnavigation/DesktopNavigation.tsx
+++ src/components/navigation/desktopnavigation/DesktopNavigation.tsx
@@ -1,13 +1,49 @@
-import { Button } from 'components/Button'
+import { primaryColor, primaryFont } from 'shared/styles/GlobalStyles'
 import { useNavigate } from 'react-router-dom'
+import { Button } from 'components/Button'
 import RoutingPath from 'routes/RoutingPath'
+import styled from 'styled-components'
+import neutral from 'shared/images/neutral.png'
+
 
 export const DesktopNavigation = () => {
 	const navigate = useNavigate()
 
 	return (
-		<div>
-			<Button text={'UPLOAD'} onClick={() => navigate(RoutingPath.uploadView)} />
-		</div>
+		<Wrapper>
+			<ButtonPlacement>
+				<Button text={'UPLOAD'} onClick={() => navigate(RoutingPath.uploadView)} />
+			</ButtonPlacement>
+			<Title>Codic Cloud</Title>
+			<Image src={neutral} />
+		</Wrapper>
 	)
 }
+
+const Wrapper = styled.div`
+	display: grid;
+	grid-template-columns: repeat(10, 1fr);
+`
+
+const Title = styled.span`
+	grid-column: 4/8;
+	grid-row: 1/1;
+	justify-self: center;
+	align-self:center;
+	font-family: ${primaryFont};
+	color: ${primaryColor};
+	font-size: 3rem;
+`
+
+const Image = styled.img`
+	width: 80px;
+	grid-column: 9/9;
+	align-self:center;
+	justify-self: center;
+`
+
+const ButtonPlacement = styled.div`
+	grid-column: 1/3;
+	justify-self: center;
+	align-self:center;
+`
--- src/view/filesearch/FileSearchView.tsx
+++ src/view/filesearch/FileSearchView.tsx
@@ -1,30 +1,12 @@
-import { primaryColor, primaryFont } from '../../shared/styles/GlobalStyles'
-import { useContext, useState } from 'react'
-import { SearchHandler } from '../../components/SearchHandler'
-import { SearchContext } from '../../shared/provider/SearchFileProvider'
-import neutral from '../../shared/images/neutral.png'
+import { useState } from 'react'
+import { SearchHandler } from 'components/SearchHandler'
 import styled from 'styled-components'
-import happy from '../../shared/images/happy.png'
-import sad from '../../shared/images/sad.png'
 
 export const FileSearchView = () => {
-	const [searchResults] = useContext(SearchContext)
 	const [searchField, setSearchField] = useState<string>('')
-
-	const determineCloud = () => {
-		if (searchField && searchResults?.length != 0) {
-			return <Image src={happy} />
-		}
-		else if (searchResults?.length == 0 && searchField) {
-			return <Image src={sad} />
-		} else {
-			return <Image src={neutral} />
-		}
-	}
 
 	return (
 		<Wrapper>
-			<Title>Codic Cloud {determineCloud()}</Title>
 			<Div>
 				<SearchHandler searchField={searchField} setSearchField={setSearchField} />
 			</Div>
@@ -39,20 +21,6 @@ const Wrapper = styled.div`
 	display: grid;
 	grid-template-columns: repeat(10, 1fr);
 	grid-template-rows: repeat(10, 1fr);
-`
-
-const Image = styled.img`
-	width: 70px;
-	padding: 2%;	
-`
-
-const Title = styled.span`
-	grid-column: 1/11;
-	grid-row: 1/1;
-	justify-self: center;
-	font-family: ${primaryFont};
-	color: ${primaryColor};
-	font-size: 3rem;
 `
 
 const Div = styled.div`
