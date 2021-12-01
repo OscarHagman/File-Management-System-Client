41f9c4329df62beb7436fe36efb6fbe1377701a0
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2021-12-01 10:06:21 +0100

    m

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { SearchFileProvider } from './shared/provider/SearchFileProvider'
-import { GlobalStyles } from './shared/styles/GlobalStyles'
 import { ToastifyConfigurations } from './shared/configurations/ToastifyConfigurations'
-import { Routing } from './routes/Routing'
+import { SearchFileProvider } from './shared/provider/SearchFileProvider'
+import { ScrollBarStyle } from 'shared/styles/ScrollBarStyle'
+import { GlobalStyles } from './shared/styles/GlobalStyles'
 import { Navigation } from 'components/navigation/Navigation'
+import { Routing } from './routes/Routing'
 
 export const App = () => {
 	return (
 		<>
 			<GlobalStyles />
+			<ScrollBarStyle />
 			<ToastifyConfigurations />
 			<SearchFileProvider>
 				<Routing>
