3c0f45c6a69598b5eefa82c09ce9ca35451dcf3e
OscarHagman <oscar.leslie.hagman@gmail.com>
2021-12-16 00:38:34 +0100

    revert back to dev

--- src/shared/api/BackendAPI.ts
+++ src/shared/api/BackendAPI.ts
@@ -3,7 +3,7 @@ import Constants from '../global/Constants'
 
 
 const BackendAPI = Axios.create({
-	baseURL: Constants.URLs.baseURL_DEMO,
+	baseURL: Constants.URLs.baseURL_DEV,
 })
 
 export default BackendAPI
